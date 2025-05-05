ee99392c714c19a3f72ea3d289a64015a8f7c62a
Sheng Qin <sqin@microfocus.com>
2025-05-05 14:40:50 +0800

    初始化项目，并导入相关依赖，注册模块beacon-api到注册中心。

--- beacon-api/src/main/java/com/sqin/beaconapi/BeaconApiApplication.java
+++ beacon-api/src/main/java/com/sqin/beaconapi/BeaconApiApplication.java
@@ -2,8 +2,10 @@ package com.sqin.beaconapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BeaconApiApplication {
 
     public static void main(String[] args) {
