37112a65ddaaf71ec2ecbc3fda09577707201f7f
Sheng Qin <sqin@microfocus.com>
2025-05-05 13:36:48 +0800

    初始化项目，父项目和子项目

---
+++ beacon-api/src/main/java/com/sqin/beaconapi/BeaconApiApplication.java
@@ -0,0 +1,13 @@
+package com.sqin.beaconapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeaconApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeaconApiApplication.class);
+    }
+
+}
---
+++ beacon-api/src/main/java/com/sqin/beaconapi/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.sqin.beaconapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test beacon api";
+    }
+
+}
