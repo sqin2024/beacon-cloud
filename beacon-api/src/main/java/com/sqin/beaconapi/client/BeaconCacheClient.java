7aba8c26d075fe7384b0331818f5de697b0c0a1d
Sheng Qin <sqin@microfocus.com>
2025-05-06 23:30:03 +0800

    直接通过CacheClient查询到的IP白名单在转换为Object类型时，直接报错。原因是缓存模块在响应数据时，响应头是【text/plain;chartset=utf-8】。导致Object类型无法直接接收。这里选择的处理方案是：
    直接给OpenFeign的方法提供一个方法重载，从而将返回值，直接设置为String，String类型可以支持【text/plain;chartset=utf-8】这个响应头

--- beacon-api/src/main/java/com/sqin/beaconapi/client/BeaconCacheClient.java
+++ beacon-api/src/main/java/com/sqin/beaconapi/client/BeaconCacheClient.java
@@ -17,4 +17,10 @@ public interface BeaconCacheClient {
     @GetMapping("/cache/hgetall/{key}")
     Map hGetAll(@PathVariable(value = "key") String key);
 
+    @GetMapping("/cache/hget/{key}/{field}")
+    Object hget(@PathVariable(value = "key") String key, @PathVariable(value = "field") String field);
+
+    @GetMapping("/cache/hget/{key}/{field}")
+    String hgetString(@PathVariable(value = "key") String key, @PathVariable(value = "field") String field);
+
 }
--- beacon-api/src/main/java/com/sqin/beaconapi/filter/impl/IPCheckFilter.java
+++ beacon-api/src/main/java/com/sqin/beaconapi/filter/impl/IPCheckFilter.java
@@ -1,8 +1,14 @@
 package com.sqin.beaconapi.filter.impl;
 
+import com.alibaba.cloud.commons.lang.StringUtils;
+import com.sqin.beaconapi.client.BeaconCacheClient;
 import com.sqin.beaconapi.filter.CheckFilter;
+import com.sqin.common.constant.CacheConstant;
+import com.sqin.common.enums.ExceptionEnums;
+import com.sqin.common.exception.ApiException;
 import com.sqin.common.model.StandardSubmit;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,9 +20,27 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class IPCheckFilter implements CheckFilter {
 
+    private static final String IP_ADDRESS = "ipAddress";
+
+    @Autowired
+    private BeaconCacheClient cacheClient;
+
     @Override
     public void check(StandardSubmit standardSubmit) {
         log.info("【接口模块-校验ip】 校验ing");
+        // 根据apikey以及ipaddress，查询客户的白名单
+        String ip = cacheClient.hgetString(CacheConstant.CLIENT_BUSINESS_PREFIX + standardSubmit.getApiKey(), IP_ADDRESS);
+        standardSubmit.setIp(ip);
+
+        //2. 如果IP白名单为null，直接放行
+        if (StringUtils.isEmpty(ip) || ip.contains(standardSubmit.getRealIP())) {
+            log.info("【接口模块-校验ip】  客户端请求IP合法！");
+            return;
+        }
+
+        //3. IP白名单不为空，并且客户端请求不在IP报名单内
+        log.info("【接口模块-校验ip】  请求的ip不在白名单内");
+        throw new ApiException(ExceptionEnums.IP_NOT_WHITE);
     }
 
 }
